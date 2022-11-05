955263ee5198c607fdfadec56cb8e955a86cae00
soroka <sorokindv88@mail.ru>
2022-11-05 22:11:52 +0300

    second digit of three

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("homework 2.1 ");
+Console.Write("put three-digit nbr: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("second digit of this nbr -> "+stringNumber[1]);
