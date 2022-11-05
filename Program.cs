7973c6218bd89eb117f9e98ce6bef6f5377b4b21
soroka <sorokindv88@mail.ru>
2022-11-05 23:18:27 +0300

    task weekdays

--- Program.cs
+++ Program.cs
@@ -1,6 +1,31 @@
-﻿Console.Clear();
+﻿/*
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
 Console.WriteLine("homework 2.1 ");
 Console.Write("put three-digit nbr: ");
 int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(threeDigitNumber);
 Console.WriteLine("second digit of this nbr -> "+stringNumber[1]);
+*/
+
+
+/*
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("task 15");
+
+Console.Write("enter digit for weekday: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+{
+    if(dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("(day-off) -> yes");
+    }
+    else Console.WriteLine("(go to work) -> no");
+}
+return dayNumber;
+*/
+
+
